5c1d6c3ad87c86ee4bce548785d8412b4c6f7d72
SevenTwelve712 <seventwwwelve712@gmail.com>
2025-05-25 12:23:04 +0300

    add some info to nginx page; add examples;

--- webreminder_app/pages/net/nginx_F.py
+++ webreminder_app/pages/net/nginx_F.py
@@ -2,6 +2,7 @@ from webreminder_app.utils.instruction import Instruction
 from webreminder_app.utils.chapter_list import define_from_html
 from webreminder_app.utils.unordered_list import UnorderedList
 from webreminder_app.utils.content_table import *
+from markupsafe import escape
 
 block_dir = """http{
     server{
@@ -128,12 +129,228 @@ location = ContentTable(
         ContentTableLine(
             [
                 'index',
-                'Задает какой файл будет возвращаться, если был задан uri, совпадающий с uri секции location. (вроде бы index это значение по дефолту)',
+                'Задает какой файл будет возвращаться, если пользователь обратился в каталог, а не к файлу',
                 'Путь'
+            ]
+        ),
+        ContentTableLine(
+            [
+                'allow',
+                'Разрешает доступ с определенных ip адресов',
+                'адрес/сеть/all'
+            ]
+        ),
+        ContentTableLine(
+            [
+                'deny',
+                'Запрещает доступ с определенных ip адресов',
+                'адрес/сеть/all'
             ]
         )
     ]
 )
+
+proxy = ContentTable(
+    False,
+    ['Директива', 'За что отвечает', 'Значение'],
+    [
+        ContentTableLine(
+            [
+                'proxy_pass',
+                'Куда будет отправляться запрос (проксируемый сервер)',
+                'Адрес (https://address:port).'
+            ]
+        ),
+        ContentTableLine(
+            [
+                'proxy_set_header',
+                'Задает заголовок http запроса, который будет отправлен проксируемому серверу',
+                escape('<заголовок> <на что меняем>. Какие заголовки нужно менять, смотри ниже')
+            ]
+        )
+    ]
+)
+
+proxy_uri = """http{
+	server{
+		listen 5001;
+		location /usual_uri {
+			proxy_pass http://127.0.0.1:5002;
+		}
+		location /without_backslash {
+			proxy_pass http://127.0.0.1:5002/;
+		}
+		location /some/prefixes/etc {
+		    proxy_pass http://127.0.0.1:5002/main_prefix/
+		}
+	}
+	server{
+		listen 5002;
+		location / {root /home/seventwelve712/www/;}
+	}
+}"""
+
+tls = ContentTable(
+    False,
+    ['Директива', 'За что отвечает', 'Значение'],
+    [
+        ContentTableLine(
+            [
+                'listen',
+                'Надо дописать ssl после порта, чтобы сказать, что вебсервер работает с использованием шифрования',
+                'port ssl'
+            ]
+        ),
+        ContentTableLine(
+            [
+                'ssl_certificate',
+                'Путь к ssl сертификату (публичный ключ)',
+                'Путь'
+            ]
+        ),
+        ContentTableLine(
+            [
+                'ssl_certificate_key',
+                'Путь к ключу сертификата (приватный ключ)',
+                'Путь'
+            ]
+        ),
+        ContentTableLine(
+            [
+                'ssl_protocols',
+                'Какие протоколы будут использоваться. На май 2025 считаются безопасными TLSv1.2 и TLSv1.3',
+                'Протоколы через пробел'
+            ]
+        ),
+        ContentTableLine(
+            [
+                'ssl_ciphers',
+                'Какие шифры tls предложит клиенту. Рекомендуется использовать эти:',
+                "'TLS_AES_256_GCM_SHA384:<br>TLS_CHACHA20_POLY1305_SHA256:<br>TLS_AES_128_GCM_SHA256:<br>ECDHE-ECDSA-AES256-GCM-SHA384:<br>ECDHE-RSA-AES256-GCM-SHA384'"
+            ]
+        ),
+        ContentTableLine(
+            [
+                'ssl_prefer_<br>server_ciphers',
+                'Делает шифры сервера предпочтительнее шифров клиента (если значение on)',
+                'on/off'
+            ]
+        )
+    ]
+)
+
+main_ex = """user www-data;
+worker_processes 1;
+pid /run/nginx.pid;
+error_log /var/log/nginx/error.log;
+include /etc/nginx/modules-enabled/*.conf;"""
+
+events_ex = """events {
+	worker_connections 8192;
+}"""
+
+http_ex = """http{
+    access_log /var/log/nginx/access.log;
+    server {}
+}"""
+
+server_ex = """server{
+    listen 80;
+    server_name webreminder712.ru;
+    return 301 https://webreminder712.ru:44344$request_uri;
+    location / {}
+}"""
+
+location_ex1 = """location /some_data{
+    root /var/www/data
+}"""
+
+proxy_ex = """location /{
+    proxy_pass http://127.0.0.1:8000/;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header X-Forwarded-Host $host;
+    proxy_set_header X-Forwarded-Prefix /;
+}"""
+
+tls_ex = """server{
+    listen 44344 ssl;
+    server_name webreminder712.ru;
+    ssl_certificate /home/seventwelve712/www/serts/cert;
+    ssl_certificate_key /home/seventwelve712/www/serts/key;
+    ssl_protocols TLSv1.3 TLSv1.2;
+    ssl_ciphers 'TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256<br>:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384';
+    ssl_prefer_server_ciphers on;
+    location / {}
+}"""
+
+mini = """events {}
+http {
+    server {
+        listen 80;
+    }
+}"""
+
+total = """user seventwelve712;
+worker_processes 1;
+pid /run/nginx.pid;
+error_log /var/log/nginx/error.log;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+	worker_connections 8192;
+}
+http {
+	# Настраиваем логи доступа
+	access_log /var/log/nginx/access.log;
+
+	# Настраиваем редирект с http на https
+	server{
+		listen 80;
+		server_name webreminder712.ru;
+		return 301 https://webreminder712.ru:44344$request_uri;
+	}
+
+	# Сам http сервер
+	server{
+		listen 44344 ssl;
+		server_name webreminder712.ru;
+
+		# настройка tls соединения
+		ssl_certificate /home/seventwelve712/www/serts/cert;
+		ssl_certificate_key /home/seventwelve712/www/serts/key;
+		ssl_protocols TLSv1.3 TLSv1.2;
+		ssl_ciphers 'TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256<br>:TLS_AES_128_GCM_SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384';
+		ssl_prefer_server_ciphers on;
+
+		# Настраиваем статусную страницу
+		location /nginx_status{
+			stub_status on;
+			access_log off;
+			# Разрешаем с локалхоста
+			allow 127.0.0.1;
+			# Запрещаем со всех остальных адресов
+			deny all;
+		}
+
+		# Настраиваем перенаправление на внутренний сервер (гуникорн)
+		location /{
+			proxy_pass http://127.0.0.1:8000/;
+			proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+			proxy_set_header X-Forwarded-Proto $scheme;
+			proxy_set_header X-Forwarded-Host $host;
+			proxy_set_header X-Forwarded-Prefix /;
+		}
+	}
+}
+"""
+
+location_status = """location /nginx_status{
+    stub_status on;
+    access_log off;
+    allow 127.0.0.1;
+    deny all;
+}"""
 
 kwargs = {
     "block_dir": block_dir,
@@ -142,6 +359,19 @@ kwargs = {
     "http": http_,
     "server": server,
     "location_ex": location_ex,
-    "location": location
+    "location": location,
+    "proxy_uri": proxy_uri,
+    "proxy": proxy,
+    "tls": tls,
+    "main_ex": main_ex,
+    "events_ex": events_ex,
+    "http_ex": http_ex,
+    "server_ex": server_ex,
+    "location_ex1": location_ex1,
+    "proxy_ex": proxy_ex,
+    "tls_ex": tls_ex,
+    "mini": mini,
+    "total": total,
+    "location_status": location_status
 }
 page = Instruction('nginx', 'nginx.html', kwargs, define_from_html('nginx.html'))
